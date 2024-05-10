8e576ed1002b408d9784629274de667760040aee
yuichi.imori <yuichi.imori@classi.jp>
2024-05-10 13:29:11 +0900

    本日の日付取得とコマンドライン引数の値を取得するコードを実装

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'optparse'
+
+# 今日の日付を取得する
+today_date =  Date.today
+date_params = {year: today_date.year, month: today_date.month, day: today_date.day}
+
+# コマンドライン引数で指定された年と月を取得する
+opt = OptionParser.new
+command_params = {}
+
+opt.on('-y [VAL]') {|v| v.to_i}
+opt.on('-m [VAL]') {|v| v.to_i}
+
+opt.parse!(ARGV, into: command_params)
