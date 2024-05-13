0dd22be1646d45f3af3e4a19f7f3aca9bf8bdbb4
yuichi.imori <yuichi.imori@classi.jp>
2024-05-13 16:01:17 +0900

    コマンドライン上の数値変換と得点加算時の挙動をメソッドとして実装

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -3,39 +3,57 @@
 score_list = ARGV[0].split(',')
 
 total = 0 # 合計得点を入力する変数
-flame = [1, 1] # フレーム、何回目かを管理する変数
+add_score = 0 # ストライク・スペアの際の加算得点を記録する変数
+flame = [1, 1] # フレーム、何投目かを管理する変数
 pre_score = 0 # 同フレームの前回のスコアを記録する変数
 strike_spare_flag = 0 # 前フレームが通常、スペア、ストライクのいずれかを判定する変数
 
+# スコアをint型に変換する
+def convert_str2int(score_s)
+  if score_s == 'X'
+    10 # Xを10に置き換える
+  else
+    score_s.to_i
+  end
+end
+
+# ストライク・スペアの時の加算得点を計算するプログラム
+def cal_addscore(score, flag)
+  if flag > 0 # rubocop:disable Style/NumericPredicate
+    add_score = score # 得点を加算
+    flag -= 1 # フラグの数を減らす
+  else
+    add_score = 0
+  end
+  return add_score, flag # rubocop:disable Style/RedundantReturn
+end
+
 # 得点加算部分の実装
 score_list.each do |score_s|
-  add_score = 0 # ストライク・スペアの際の加算得点を記録する変数
+  score_i = convert_str2int(score_s) # str型の数値をint型に変換する
   if score_s == 'X' # ストライクだった時の処理
-    score = 10 # Xを10に置き換える
-    # 前回がストライクないしスペアであった際には得点を+10する
-    add_score = 10 if strike_spare_flag > 0 # rubocop:disable Style/NumericPredicate
-    total += add_score + score # 得点を加算
+    # 前回がストライク・スペアの確認をする
+    add_score, strike_spare_flag = cal_addscore(score_i, strike_spare_flag)
+    total += add_score + score_i
     flame[0] += 1 # フレームを更新
     strike_spare_flag = 2
     next
   end
-  # 1.前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
-  if strike_spare_flag > 0 # rubocop:disable Style/NumericPredicate
-    add_score = score_s.to_i
-    strike_spare_flag -= 1
-  end
 
-  # 3.投球数、フレーム数の更新
+  # 前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
+  add_score, strike_spare_flag = cal_addscore(score_i, strike_spare_flag)
+
+  # 投球数、フレーム数の更新
   if flame[1] == 1
     flame[1] = 2
-    pre_score = score_s.to_i
+    pre_score = score_i
   else
-    strike_spare_flag = 1 if pre_score + score_s.to_i == 10 # スペアになるかを判定し、該当する場合はフラグを1にする
+    strike_spare_flag = 1 if pre_score + score_i == 10 # スペアになるかを判定し、該当する場合はフラグを1にする
     flame[0] += 1
     flame[1] = 1
     pre_score = 0
   end
-  total += add_score + score_s.to_i # 得点を加算
+  total += add_score + score_i # 得点を加算
 end
 
 puts total
