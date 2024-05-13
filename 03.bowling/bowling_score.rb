873dfbcf20ac74acf05a49f4b5be1aa4c1509fd8
yuichi.imori <yuichi.imori@classi.jp>
2024-05-13 16:37:22 +0900

    加算処理を最初に移動させてコードを簡潔化

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -31,10 +31,11 @@ end
 # 得点加算部分の実装
 score_list.each do |score_s|
   score_i = convert_str2int(score_s) # str型の数値をint型に変換する
+  # 前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
+  add_score, strike_spare_flag = cal_addscore(score_i, strike_spare_flag)
 
   # 10フレーム目の処理を別で実装
   if flame[0] == 10
-    add_score, strike_spare_flag = cal_addscore(score_i, strike_spare_flag) # 前フレームでの加算得点処理
     total += add_score + score_i # 得点を加算
     next
   end
@@ -42,15 +43,11 @@ score_list.each do |score_s|
   # ストライクだった時の処理
   if score_s == 'X'
     # 前回がストライク・スペアの確認をする
-    add_score, strike_spare_flag = cal_addscore(score_i, strike_spare_flag)
     total += add_score + score_i
     flame[0] += 1 # フレームを更新
     strike_spare_flag = 2
     next
   end
-
-  # 前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
-  add_score, strike_spare_flag = cal_addscore(score_i, strike_spare_flag)
 
   # 投球数、フレーム数の更新
   if flame[1] == 1 # 1投目
