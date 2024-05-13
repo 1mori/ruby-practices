d01afb76869abb4d595e9805dfe2a6ae688acafa
yuichi.imori <yuichi.imori@classi.jp>
2024-05-13 16:59:57 +0900

    if文処理を1行に圧縮

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -32,21 +32,19 @@ def cal_addscore(score, flag, running_strike)
 end
 
 # 得点加算部分の実装
-score_list.each do |score_s| # rubocop:disable Metrics/BlockLength
+score_list.each do |score_s|
   score_i = convert_str2int(score_s) # str型の数値をint型に変換する
   # 前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
   add_score, strike_spare_flag, running_strike = cal_addscore(score_i, strike_spare_flag, running_strike)
 
+  # 得点を合計得点に加算
+  total += add_score + score_i
+
   # 10フレーム目の処理を別で実装
-  if flame[0] == 10
-    total += add_score + score_i # 得点を加算
-    next
-  end
+  next if flame[0] == 10
 
   # ストライクだった時の処理
   if score_s == 'X'
-    # 前回がストライク・スペアの確認をする
-    total += add_score + score_i
     flame[0] += 1 # フレームを更新
     strike_spare_flag = 2
     running_strike = true if pre_strike == true
@@ -66,9 +64,6 @@ score_list.each do |score_s| # rubocop:disable Metrics/BlockLength
     flame[1] = 1
     pre_score = 0
   end
-
-  # 得点を加算
-  total += add_score + score_i
 end
 
 puts total
