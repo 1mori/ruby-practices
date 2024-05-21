ac6c0000f24553509b9adce73547b448dfc3303c
yuichi.imori <yuichi.imori@classi.jp>
2024-05-21 16:26:22 +0900

    変数running_strikeを削除

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -2,28 +2,27 @@
 
 total = 0
 flame = [1, 1]
-running_strike = false
 strike_spare_flag = 0
 
 score_list = ARGV[0].split(',')
 score_list = score_list.map { |score| score == 'X' ? 10 : score.to_i }
 
 # ストライク・スペアの時の加算得点を計算するプログラム
-def cal_addscore(score, flag, running_strike)
+def cal_addscore(score, flag, score_list, ball_number)
   if flag > 0 # rubocop:disable Style/NumericPredicate
     add_score = score # 得点を加算
     flag -= 1 # フラグの数を減らす
-    add_score += score if running_strike
+    add_score += score if score_list[ball_number - 1] && score_list[ball_number - 2]
   else
     add_score = 0
   end
-  [add_score, flag, false]
+  [add_score, flag]
 end
 
 # 得点加算部分の実装
 score_list.each_with_index do |score, ball_number|
   # 前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
-  add_score, strike_spare_flag, running_strike = cal_addscore(score, strike_spare_flag, running_strike)
+  add_score, strike_spare_flag,  = cal_addscore(score, strike_spare_flag, score_list, ball_number)
 
   # 得点を合計得点に加算
   total += add_score + score
@@ -34,7 +33,6 @@ score_list.each_with_index do |score, ball_number|
   if score == 10
     flame[0] += 1 # フレームを更新
     strike_spare_flag = 2
-    running_strike = true if score_list[ball_number - 1] == 10
     next
   end
 
