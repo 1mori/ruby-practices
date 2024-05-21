e73488491c2024489e9f262e552e7d0d86b2a40e
yuichi.imori <yuichi.imori@classi.jp>
2024-05-21 15:43:42 +0900

    score_listを予めintに変換するようにしたため、score_i, score_sの記載を削除

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -23,19 +23,18 @@ def cal_addscore(score, flag, running_strike)
 end
 
 # 得点加算部分の実装
-score_list.each do |score_s|
-  score_i = convert_str2int(score_s) # str型の数値をint型に変換する
+score_list.each_with_index do |score, ball_number|
   # 前フレームがスペアないしストライクかどうかを判定し、いずれかに該当する場合は得点を加算する
-  add_score, strike_spare_flag, running_strike = cal_addscore(score_i, strike_spare_flag, running_strike)
+  add_score, strike_spare_flag, running_strike = cal_addscore(score, strike_spare_flag, running_strike)
 
   # 得点を合計得点に加算
-  total += add_score + score_i
+  total += add_score + score
 
   # 10フレーム目の処理を別で実装
   next if flame[0] == 10
 
   # ストライクだった時の処理
-  if score_s == 'X'
+  if score == 'X'
     flame[0] += 1 # フレームを更新
     strike_spare_flag = 2
     running_strike = true if pre_strike == true
