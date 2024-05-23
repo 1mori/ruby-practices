407ff939608b161d704794d4444f829b00bbb653
yuichi.imori <yuichi.imori@classi.jp>
2024-05-23 18:59:50 +0900

    取得した要素を最大の長さが3の配列に分割するsplit_arrayメソッドを追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,8 +10,25 @@ def except_hidden_file(entries)
   entries.reject { |entry| entry[0] == '.' }
 end
 
+def split_array(array, max_length)
+  num_rows = (array.size.to_f / max_length).ceil
+  shaped_array = Array.new(num_rows) { [] }
+
+  array.each_with_index do |element, index|
+    row_index = index % num_rows
+    shaped_array[row_index] << element
+  end
+  shaped_array
+end
+
 entries = sort_entries(entries)
 entries = except_hidden_file(entries)
+shaped_array = split_array(entries, 3)
+
+
+
+
+p shaped_array
 
 entries.each do |entry|
   puts entry
