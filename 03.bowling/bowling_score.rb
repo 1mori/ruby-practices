45be9b51f52c8b695432d37c28886b2667356dbe
yuichi.imori <yuichi.imori@classi.jp>
2024-05-23 16:18:06 +0900

    calculate_add_scoreに渡す値を計算に必要な要素に絞ってscore_groupに格納

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -1,28 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-total = 0
 frame = 1
 throw = 1
 
 score_list = ARGV[0].split(',')
 score_list = score_list.map { |score| score == 'X' ? 10 : score.to_i }
 
-# ストライク・スペアの時の加算得点を計算するプログラム
-def calculate_add_score(throw, score, score_list, ball_number)
+# ストライク・スペアの時の加算得点を計算
+def calculate_add_score(throw, score_group)
   add_score = 0
-  add_score += score_list[ball_number + 1] + score_list[ball_number + 2] if throw == 1 && score == 10 # ストライク判定
-  add_score += score_list[ball_number + 1] if throw == 2 && score_list[ball_number - 1] + score == 10 # スペア判定
+  add_score += score_group[1] + score_group[2] if throw == 1 && score_group[0] == 10 # ストライク判定
+  add_score += score_group[2] if throw == 1 && score_group[0] + score_group[1] == 10 # スペア判定
 
   add_score
 end
 
-# 得点加算部分の実装
-total = score_list.each_with_index.sum do |score, ball_number|
+total = score_list.each_with_index.sum do |score, index|
+  score_group = score_list[index, 3]
   add_score = if frame == 10
                 0
               else
-                calculate_add_score(throw, score, score_list, ball_number)
+                calculate_add_score(throw, score_group)
               end
 
   # 投球数、フレーム数の更新
