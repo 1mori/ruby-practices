68ca318d0f905700cb7e4c7958af2014ea59882a
yuichi.imori <yuichi.imori@classi.jp>
2024-05-28 11:11:22 +0900

    フレーム数・投球数の更新をupdate_game_state内で実行するように変更

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -11,28 +11,24 @@ def update_game_state(frame, throw, score_group)
   add_score = 0
   if throw == 1 && score_group[0] == 10 # ストライク判定
     add_score += score_group[1..2].sum
-  end
-  if throw == 1 && score_group[0..1].sum == 10 # スペア判定
+    frame += 1
+    throw = 1
+  elsif throw == 1 && score_group[0..1].sum == 10 # スペア判定
     add_score += score_group[2]
+    throw = 2
+  else
+    frame += 1 if throw == 2
+    throw = (throw == 2 ? 1 : 2)
   end
 
-  add_score
+  [add_score, frame, throw]
 end
 
 total = score_list.each_with_index.sum do |score, index|
-  score_group = score_list[index, 3]
-  if frame == 10
-    next score
-  else
-    add_score, frame, throw = update_game_state(frame, throw, score_group)
-  end
+  next score if frame == 10
 
-  if throw == 1 && score == 10 || throw == 2
-    frame += 1
-    throw = 1
-  else
-    throw = 2
-  end
+  score_group = score_list[index, 3]
+  add_score, frame, throw = update_game_state(frame, throw, score_group)
 
   add_score + score
 end
