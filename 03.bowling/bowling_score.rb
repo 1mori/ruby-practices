1c730789d151ab8d109f39b5e9aed734a075c46f
yuichi.imori <yuichi.imori@classi.jp>
2024-05-30 12:19:52 +0900

    update_game_state内の処理を呼び出し元に戻してメソッドを削除

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -7,7 +7,10 @@ throw = 1
 score_list = ARGV[0].split(',')
 score_list = score_list.map { |score| score == 'X' ? 10 : score.to_i }
 
-def update_game_state(frame, throw, score_group)
+total = score_list.each_with_index.sum do |score, index|
+  next score if frame == 10
+
+  score_group = score_list[index, 3]
   add_score = 0
   if throw == 1 && score_group[0] == 10 # ストライク判定
     add_score += score_group[1..2].sum
@@ -19,15 +22,6 @@ def update_game_state(frame, throw, score_group)
     frame += 1 if throw == 2
     throw = (throw == 2 ? 1 : 2)
   end
-
-  [add_score, frame, throw]
-end
-
-total = score_list.each_with_index.sum do |score, index|
-  next score if frame == 10
-
-  score_group = score_list[index, 3]
-  add_score, frame, throw = update_game_state(frame, throw, score_group)
 
   add_score + score
 end
