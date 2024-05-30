49a06568f844f67504d5738e1e1403247ba16fed
yuichi.imori <yuichi.imori@classi.jp>
2024-05-30 12:54:27 +0900

    ストライク・スペア・その他判定の最適化

--- 03.bowling/bowling_score.rb
+++ 03.bowling/bowling_score.rb
@@ -11,20 +11,18 @@ total = score_list.each_with_index.sum do |score, index|
   next score if frame == 10
 
   score_group = score_list[index, 3]
-  add_score = 0
-  if throw == 1 && score_group[0] == 10 # ストライク判定
-    add_score += score_group[1..2].sum
-    frame += 1
-  elsif throw == 1 && score_group[0..1].sum == 10 # スペア判定
-    add_score += score_group[2]
-    throw = 2
-  else
-    if throw == 2
+  if throw == 1
+    if score_group[0] == 10 # ストライク判定
+      add_score = score_group[1..2].sum
       frame += 1
-      throw = 1
     else
+      add_score = (score_group[0..1].sum == 10 ? score_group[2] : 0) # スペアかそうでないか判定
       throw = 2
     end
+  else
+    add_score = 0
+    frame += 1
+    throw = 1
   end
 
   add_score + score
