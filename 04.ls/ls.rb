48eeed0e0453b9f84a4b61b2a9d6ede24be05725
yuichi.imori <yuichi.imori@classi.jp>
2024-05-31 11:37:59 +0900

    メソッドshaped_file_pathsを定義、これまでのsplit_arrayと挙動は同様だがより簡潔な処理に変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,22 +10,21 @@ def except_hidden_file(file_paths)
   file_paths.reject { |element| element[0] == '.' }
 end
 
-def split_array(array, max_length)
-  num_rows = (array.size.to_f / max_length).ceil
-  shaped_array = Array.new(num_rows) { [] }
+def shaped_file_paths(file_paths)
+  return [] if file_paths.empty?
 
-  array.each_with_index do |element, index|
-    row_index = index % num_rows
-    shaped_array[row_index] << element
-  end
-  shaped_array
+  num_rows = (file_paths.size.to_f / MAX_CHUNK).ceil
+  sliced_paths = file_paths.each_slice(num_rows).to_a
+
+  padded_slices = sliced_paths.map { |slice| slice + [nil] * (num_rows - slice.length) }
+  padded_slices.transpose.map(&:compact)
 end
 
 entries = entries.sort
 entries = except_hidden_file(entries)
 
+shaped_array = shaped_file_paths(entries)
 max_string_length = entries.map(&:length).max
-shaped_array = split_array(entries, 3)
 
 shaped_array.each do |array_element|
   array_element.each do |element|
