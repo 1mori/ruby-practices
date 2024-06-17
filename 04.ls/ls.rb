e2dc4f70b062959902ac9ed76239fe89f69bc82c
yuichi.imori <yuichi.imori@classi.jp>
2024-06-17 10:53:56 +0900

    add: lオプションが指定された時の分岐となるif文をひとまず作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -28,17 +28,21 @@ end
 
 file_paths = except_hidden_file(file_paths)
 
-num_rows = (file_paths.size.to_f / MAX_CHUNK).ceil
-sliced_paths = chunk_file_paths(file_paths, num_rows)
-shaped_file_paths_array = transpose_chunks(sliced_paths, num_rows)
+if options['l']
+  p 'l'
+else
+  num_rows = (file_paths.size.to_f / MAX_CHUNK).ceil
+  sliced_paths = chunk_file_paths(file_paths, num_rows)
+  shaped_file_paths_array = transpose_chunks(sliced_paths, num_rows)
 
-max_string_length = file_paths.map(&:length).max
+  max_string_length = file_paths.map(&:length).max
 
-shaped_file_paths_array.each do |array_element|
-  array_element.each do |element|
-    tab_count = ((max_string_length - element.length) / TAB_SPACE.to_f).ceil + 1 # タブの挿入する回数を計算
-    print element
-    print "\t" * tab_count
+  shaped_file_paths_array.each do |array_element|
+    array_element.each do |element|
+      tab_count = ((max_string_length - element.length) / TAB_SPACE.to_f).ceil + 1 # タブの挿入する回数を計算
+      print element
+      print "\t" * tab_count
+    end
+    puts
   end
-  puts
 end
