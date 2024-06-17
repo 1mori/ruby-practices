e3af6bfef0b39470e8cd6b5cefd9ce3005fb5355
yuichi.imori <yuichi.imori@classi.jp>
2024-06-17 12:59:48 +0900

    add: 表示するファイルモードの文字列を作成するメソッドconvert_file_modeを作成
    8進数に変換したファイルモードの数値列から、lsコマンドで表示する文字列形式に変換するメソッド
    - ファイルの種類はfile_mode.ftypeから取得し、予め作成しているFILE_TYPEのhashから対応する文字をとってくる
    - ファイルの読み書き権限などは8進数数値列の4~6番目が対応しているため、その部分を取り出してFILE_MODEのhashから対応する文字列を取得した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -43,6 +43,13 @@ def transpose_chunks(sliced_paths, num_rows)
   padded_slices.transpose.map(&:compact)
 end
 
+def convert_file_mode(file_stat)
+  file_mode_octal = file_stat.mode.to_s(8).rjust(6, '0')
+  file_mode = FILE_TYPE[file_stat.ftype].to_s
+  file_permissions = file_mode_octal.slice(3..-1).chars.map { |char| FILE_MODE[char] }.join
+  file_mode + file_permissions
+end
+
 file_paths = Dir.entries('.').sort
 
 opt = OptionParser.new
@@ -53,7 +60,7 @@ file_paths = except_hidden_file(file_paths)
 if options['l']
   file_paths.each do |file_path|
     file_stat = File.lstat(file_path)
-    file_mode_octal = file_stat.mode.to_s(8).rjust(6, '0')
+    file_mode = convert_file_mode(file_stat)
     nlink = file_stat.nlink
     uid = Etc.getpwuid(file_stat.uid).name
     gid = Etc.getgrgid(file_stat.gid).name
